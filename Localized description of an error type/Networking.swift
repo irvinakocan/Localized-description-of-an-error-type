a1cb369f8f7829be36687e365d2f41effbd8d8c3
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-12-18 20:27:17 +0100

    Implemented getUsers function

---
+++ Localized description of an error type/Networking.swift
@@ -0,0 +1,39 @@
+//
+//  Networking.swift
+//  Localized description of an error type
+//
+//  Created by Macbook Air 2017 on 18. 12. 2023..
+//
+
+import Foundation
+import UIKit
+
+let GET_USERS_ENDPOINT = "https://jsonplaceholder.typicode.com/users"
+
+func getUsers() async throws -> [User] {
+    
+    guard let url = URL(string: GET_USERS_ENDPOINT) else {
+        throw NetworkError.invalidURL
+    }
+    
+    var result: (data: Data, response: URLResponse)
+    do {
+        result = try await URLSession.shared.data(from: url)
+    }
+    catch {
+        throw NetworkError.httpConnectionError
+    }
+    
+    guard let response = result.response as? HTTPURLResponse, response.statusCode == 200 else {
+        throw NetworkError.invalidResponseError
+    }
+    
+    var users = [User]()
+    do {
+        users = try JSONDecoder().decode([User].self, from: result.data)
+        return users
+    }
+    catch {
+        throw NetworkError.decodingError
+    }
+}
