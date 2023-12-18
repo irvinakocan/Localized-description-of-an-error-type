8720ff05f87df19dbd4eb8fd0f770a7d75bfb129
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-12-18 19:46:18 +0100

    Initial Commit

---
+++ Localized description of an error type/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Localized description of an error type
+//
+//  Created by Macbook Air 2017 on 18. 12. 2023..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
