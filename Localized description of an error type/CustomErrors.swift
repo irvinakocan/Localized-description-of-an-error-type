3d6ef86fc5a063f7b09a46f365f92e45fbb7ab77
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-12-18 21:41:19 +0100

    Custom error descriptions
    Implemented LocalizedError protocol conformance for our custom errors and custom error description for every error case.

--- Localized description of an error type/CustomErrors.swift
+++ Localized description of an error type/CustomErrors.swift
@@ -10,6 +10,25 @@ import Foundation
 enum NetworkError: Error {
     case invalidURL
     case httpConnectionError
-    case invalidResponseError
+    case invalidResponseError(code: Int)
     case decodingError
+}
+
+extension NetworkError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+            
+        case .invalidURL:
+            return NSLocalizedString("There is a problem with a URL, such as an invalid URL or a timeout.", comment: "Invalid URL")
+            
+        case .httpConnectionError:
+            return NSLocalizedString("There is an issue with an HTTP request or there is no internet connection", comment: "HTTP/Connection Error")
+            
+        case .invalidResponseError(let code):
+            return NSLocalizedString("The server responds with an unexpected format with the status code \(code).", comment: "Invalid Response Error")
+            
+        case .decodingError:
+            return NSLocalizedString("The data received from the server is unable to be decoded as the expected type.", comment: "Decoding Error")
+        }
+    }
 }
