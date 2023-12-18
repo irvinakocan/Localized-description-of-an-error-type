ba37f5ea922a01bf57a32f2979b92372634f4193
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-12-18 19:57:56 +0100

    Implemented a model in a new file

---
+++ Localized description of an error type/Models.swift
@@ -0,0 +1,14 @@
+//
+//  Models.swift
+//  Localized description of an error type
+//
+//  Created by Macbook Air 2017 on 18. 12. 2023..
+//
+
+import Foundation
+
+struct User {
+    let name: String
+    let username: String
+    let email: String
+}
